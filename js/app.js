09b04d93f667dd28d77d1492cd6c37a188b18997
Gary Barkauski <gbarka2@gmail.com>
2020-12-18 15:36:25 -0700

    Add AJAX and simplify data

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-console.log("hello world");
+//PULL AND RENDER DATA FROM GOOGLE SHEET//
+
+$.ajax('https://spreadsheets.google.com/feeds/list/1Gw8eY-blGjaksHJJEZ_J94TYduwKqWvDPcdzm8uJunM/1/public/full?alt=json')
+.then((data) => {
+  //checking my data
+  // console.log(data)
+
+  //put project in a variable
+  const rawProjects = data.feed.entry
+  // console.log(rawProjects)
+  //beautify our projects array
+  const projects = rawProjects.map((project) => {
+    return{
+      name: project.gsx$name.$t,
+      img: project.gsx$img.$t,
+      description: project.gsx$description.$t,
+      live: project.gsx$live.$t,
+      github: project.gsx$github.$t
+    }
+  })
+  console.log(projects)
+
+  //use jQuery to render projects to page//
+
+
+
+
+
+
+  ////////////////////////////////////////////
+})
+
+
+//PULL DATA FROM BLOG FROM HEADLESS CMS//
