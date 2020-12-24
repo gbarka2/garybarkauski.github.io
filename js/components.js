96311c8801fb2fd8ed9cd883e09080da06943772
Gary Barkauski <gbarka2@gmail.com>
2020-12-23 21:29:27 -0700

    Correct sizing on contact images on bottom

--- js/components.js
+++ js/components.js
@@ -2,9 +2,9 @@ class Project extends HTMLElement {
   constructor() {
     super()
     this.innerHTML = `
-    <img>${this.getAttribute("img")}
+    
     <p>${this.getAttribute("name")}</p>
-    <a>${this.getAttribute("live")}</a>
+    
     `
   }
   //add code that creates the project
