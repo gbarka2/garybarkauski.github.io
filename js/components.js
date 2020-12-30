9e3289a4ad00007a111876de14e88247fae3a4f7
Gary Barkauski <gbarka2@gmail.com>
2020-12-30 12:34:09 -0700

    Remove unused function

--- js/components.js
+++ js/components.js
@@ -6,12 +6,7 @@ class Project extends HTMLElement {
       <i class="fab fa-github-alt github"></i>
       <p>${this.getAttribute("name")}</p>
     </div>
-    
     `
   }
-  openLink() {
-    window.open(`${this.getAttribute("link")}`)
-  }
-  
 }
 customElements.define("project-example", Project)
